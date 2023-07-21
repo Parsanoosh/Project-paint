9ab4906f3f1aa7b3ce96b739eb696e0b32788c28
Parsa Noosh <phaji400463118@gmail.com>
2023-07-21 20:35:47 +0330

    fixed UI.

--- src/main/java/org/example/GridFactory.java
+++ src/main/java/org/example/GridFactory.java
@@ -9,14 +9,19 @@ import com.almasb.fxgl.entity.Spawns;
 import javafx.geometry.Point2D;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.StrokeType;
 
 public class GridFactory implements EntityFactory {
     private static final int BLOCK_SIZE = 20;
 
     @Spawns("cell")
     public Entity newCell(SpawnData data) {
+        var view = new Rectangle(BLOCK_SIZE, BLOCK_SIZE, Color.WHITE);
+        view.setStroke(Color.BLACK);
+        view.setStrokeType(StrokeType.INSIDE);
+        view.setStrokeWidth(0.4);
         return FXGL.entityBuilder(data)
-                .view(new Rectangle(BLOCK_SIZE,BLOCK_SIZE,Color.LIGHTGRAY))
+                .view(view)
                 .with(new CellComponent())
                 .build();
     }
@@ -25,7 +30,6 @@ public class GridFactory implements EntityFactory {
     public Entity newPlayer(SpawnData data) {
         return FXGL.entityBuilder(data)
                 .view("steve.png")
-                //.view(new Rectangle(20, 20, Color.RED))
                 .zIndex(100)
                 .build();
     }
